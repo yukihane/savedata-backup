c85f373e256fea198ac69a8c659e69584e2407db
yukihane <yukihane.feather@gmail.com>
2023-12-09 11:00:03 +0900

    anyhow を利用したエラーハンドリング

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::path::Path;
+
+use anyhow::{Context, Result};
 use directories::UserDirs;
 
-fn main() {
-    let user_dir = UserDirs::new().unwrap();
-    let dir = user_dir.document_dir().unwrap();
+fn main() -> Result<()> {
+    let user_dir = UserDirs::new().context("Not found: user_dir")?;
+    let dir: &Path = user_dir.document_dir().context("Not found: document_dir")?;
+
+    println!("{:?}", dir);
+
+    Ok(())
 }
