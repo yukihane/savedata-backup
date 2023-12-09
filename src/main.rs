685c146a73f82757db6e74d879512a219e29c7ec
yukihane <yukihane.feather@gmail.com>
2023-12-09 13:11:51 +0900

    walkdirを用いてディレクトリをトラバース

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::path::Path;
-
 use anyhow::{Context, Result};
 use directories::UserDirs;
+use walkdir::WalkDir;
 
 fn main() -> Result<()> {
     let user_dir = UserDirs::new().context("Not found: user_dir")?;
-    let dir: &Path = user_dir.document_dir().context("Not found: document_dir")?;
+    let dir = user_dir.document_dir().context("Not found: document_dir")?;
 
-    println!("{:?}", dir);
+    for entry in WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| {
+            e.file_name()
+                .to_string_lossy()
+                .to_lowercase()
+                .contains("sav")
+        })
+    {
+        // println!("{}", entry.path().display());
+        println!("{:?}", entry.file_type());
+    }
 
     Ok(())
 }
